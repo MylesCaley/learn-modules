0d466303144562aca32a427558196f194dbb8870
MylesCaley <myles.caley@gmail.com>
2016-02-12 16:05:57 -0500

    fixed scoping issue

--- Module2.js
+++ Module2.js
@@ -1,7 +1,8 @@
 function Module2() {
+	self = this;
 	this.data = "module 2 data";
 	console.log('Module2 constructor');
-	setTimeout(function(){ this.data = "module 2 ***MODIFIED*** data" }, 2000);
+	setTimeout(function(){ self.data = "module 2 ***MODIFIED*** data" }, 2000);
 }
 
 module.exports = Module2;
