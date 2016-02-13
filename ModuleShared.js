64ac0614ea44c025584334742c26993790ef8721
MylesCaley <myles.caley@gmail.com>
2016-02-13 09:16:16 -0500

    added prototype function to example

--- ModuleShared.js
+++ ModuleShared.js
@@ -1,6 +1,10 @@
-function ModuleShared(module1, module2) {
+var ModuleShared = function(module1, module2) {
   console.log('ModuleShared constructor -- ' + module1.data + ' -- ' + module2.data);
   setTimeout(function(){ console.log(module2.data) }, 4000);
+}
+
+ModuleShared.prototype.sharedFunction = function() {
+	console.log('someone called the shared function');
 }
 
 module.exports = ModuleShared;
--- index.js
+++ index.js
@@ -7,3 +7,5 @@ var ModuleShared = require('./ModuleShared.js')
 var module1 = new Module1();
 var module2 = new Module2();
 var moduleshared = new ModuleShared(module1,module2);
+
+moduleshared.sharedFunction();
